80442ecd805d5837f55b15a3a42e4de9fa3df45e
Mandragora <royer01gomez@gmail.com>
2021-07-29 11:30:32 -0400

    .

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\Controller;
+use App\Models\User;
 use Psr\Http\Message\{
     ServerRequestInterface as Request,
     ResponseInterface as Response
@@ -17,7 +18,16 @@ class AuthController extends Controller
         return $this->c->get('view')->render($response, 'auth/signup.twig');
     }
 
-    public function signup(Request $request, Response $response, $args) {
+    public function signup(Request $request, Response $response, $args)
+    {
+        User::create([
+            'name' => $request->getParsedBody()->username,
+            'email' => $request->getParsedBody()->email,
+            'password' => password_hash($request->getParsedBody()->password, PASSWORD_DEFAULT),
+        ]);
 
+        return $response
+            ->withHeader('Location', '/')
+            ->withStatus(302);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Controllers\HomeController;
 use App\Controllers\AuthController;
 
-$app->get('/', HomeController::class . ':index');
+$app->get('/', HomeController::class . ':index')->setName('home');
 $app->get('/signup', AuthController::class . ':index')->setName('auth.signup');
 $app->post('/signup', AuthController::class . ':signup');
